af8b784bc15f99a0bec163a87c600aefe8416fbe
tomek-skrond <tomek2.skrond@gmail.com>
2020-09-18 14:44:22 +0200

    commit

--- transformaty/transformaty-xml/Program.cs
+++ transformaty/transformaty-xml/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Xml.Linq;
 
 namespace transformaty_xml
 {
     class Program
     {
+        public static List<string> GetTreeContents()
+        {
+
+        }
+        public static XElement ConstructTree(List<string> treeContents)
+        {
+            var counter = 0;
+            return new XElement("dokumenty",
+                        new XElement("faktura",
+                            new XElement("kontrahentAdres1",treeContents[counter++])));
+        }
+
+            
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
